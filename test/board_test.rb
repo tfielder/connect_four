8c49b86febe72791290b4c0c379085c4cc609c4e
T Fielder <fiel1139@hotmail.com>
2018-07-13 20:27:10 -0600

    adds test for board existence.

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,9 +1,11 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "./lib/board.rb"
 
 class BoardTest < Minitest::Test
   def test_board_exists
-
+    board = Board.new
+    assert_instance_of Board, board
   end
 
   def test_print_board
