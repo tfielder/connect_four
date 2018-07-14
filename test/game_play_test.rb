6ad9f8fb2df9b8f184305c335ff23249ca3ac3da
T Fielder <fiel1139@hotmail.com>
2018-07-13 20:45:21 -0600

    adds test for game play creation.

--- test/game_play_test.rb
+++ test/game_play_test.rb
@@ -1,8 +1,10 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "./lib/game_play.rb"
 
 class GamePlayTest < Minitest::Test
   def test_it_exists
-    
+    game = GamePlay.new
+    assert_instance_of GamePlay, game
   end
 end
