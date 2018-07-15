8a312f727d9232d622b72c0bdc45610774c85195
T Fielder <fiel1139@hotmail.com>
2018-07-15 14:35:05 -0600

    adds loop for playing only one player in the connect_four runner file.

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -14,27 +14,24 @@ game = GamePlay.new(board)
 print "\n"
 puts game.return_welcome_message
 
-
+column = "y"
 #
+while column != "-1"
+  #round loop . . . .
+  #print board
+  puts game.return_board
+  #prompt for move
+  puts game.request_move
+  #get move and validate it
+  column = game.get_move
+  #add move to board
+  game.board.add_token_to_board(column, "X")
+  #print the board
+  puts game.request_move
+  #check for win, #check for tie
+  #prompt waiting message
+  #computer selects
+  #move added
+  #check for win, #check for tie
 
-#round loop . . . .
-#print board
-puts game.return_board
-#prompt for move
-puts game.request_move
-#get move and validate it
-game.get_move
-
-#add move to board
-
-#print the board
-
-#check for win, #check for tie
-
-#prompt waiting message
-
-#computer selects
-
-#move added
-
-#check for win, #check for tie
+end
