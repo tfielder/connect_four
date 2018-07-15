741377234f1e2bf5de5079cc0a4d197645f4b1cf
T Fielder <fiel1139@hotmail.com>
2018-07-15 09:40:55 -0600

    adds pseudocode to connect_four game play.

---
+++ lib/connect_four.rb
@@ -0,0 +1,19 @@
+
+
+###while loop to start / end GamePlay
+
+#Welcome message
+
+#choose token type
+#
+
+#round loop . . . .
+#print board
+#prompt for move
+#get move and validate it
+#print the board
+#check for win, #check for tie
+#prompt waiting message
+#computer selects
+#move added
+#check for win, #check for tie
