c475b14e32d81c31833607827ebaf6242c01ca8a
Tim Fielder <fiel1139@gmail.com>
2018-07-18 16:10:29 -0600

    adds the player tests for future Player Class.

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,8 +1,15 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "./lib/player.rb"
 
 class PlayerTest < Minitest::Test
   def test_it_exists
+    player = Player.new
+    assert_instance_of Player, player
+  end
 
+  def test_has_attributes
+    player = Player.new
+    assert_equal "X", player.token
   end
 end
